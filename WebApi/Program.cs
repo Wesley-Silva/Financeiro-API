eebe120381f5ae5234624852c36acefe37e73b84
Wesley-Silva <wesleybf3@gmail.com>
2024-01-26 20:58:49 -0300

    Json Web Token (JWT) e Api de User com identity

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -9,7 +9,11 @@ using Entities.Entidades;
 using Infra.Configuracao;
 using Infra.Repositorio;
 using Infra.Repositorio.Generics;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using WebApi.Token;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +23,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "API - Sistema Financeiro",
+        Description = "API - Sistema Financeiro",
+        Contact = new OpenApiContact() { Name = "Wesley Silva", Email = "teste@gmail.com" },
+        License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licences/MIT") }
+    });
+});
 
 builder.Services.AddDbContext<ContextBase>(options =>
     options.UseSqlServer(
@@ -40,16 +55,52 @@ builder.Services.AddSingleton<IDespesaServico, DespesaServico>();
 builder.Services.AddSingleton<ISistemaFinanceiroServico, SistemaFinanceiroServico>();
 builder.Services.AddSingleton<IUsuarioSistemaFinanceiroServico, UsuarioSistemaFinanceiroServico>();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+             .AddJwtBearer(option =>
+             {
+                 option.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateIssuer = false,
+                     ValidateAudience = false,
+                     ValidateLifetime = true,
+                     ValidateIssuerSigningKey = true,
+
+                     ValidIssuer = "Teste.Securiry.Bearer",
+                     ValidAudience = "Teste.Securiry.Bearer",
+                     IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678")
+                 };
+
+                 option.Events = new JwtBearerEvents
+                 {
+                     OnAuthenticationFailed = context =>
+                     {
+                         Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                         return Task.CompletedTask;
+                     },
+                     OnTokenValidated = context =>
+                     {
+                         Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                         return Task.CompletedTask;
+                     }
+                 };
+             });
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{ 
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
---
+++ WebApi/Token/TokenJWT.cs
@@ -0,0 +1,18 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace WebApi.Token
+{
+    public class TokenJWT
+    {
+        private JwtSecurityToken token;
+
+        public TokenJWT(JwtSecurityToken token)
+        {
+            this.token = token;
+        }
+
+        public DateTime ValidTo => token.ValidTo;
+
+        public string valeu => new JwtSecurityTokenHandler().WriteToken(this.token);
+    }
+}
